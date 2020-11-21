c88e1e8b3dc122a7a8c398bc352757de69713379
Kanav Bengani <kbengani21@gmail.com>
2020-11-21 17:58:29 -0600

    author's top 5 books

--- index.js
+++ index.js
@@ -12,34 +12,41 @@ bot.once("ready", () => {
 });
 
 bot.on("message", async message => {
-	if (message.content.startsWith(prefix + 'book', 0)) {
-		const args = message.content.slice(5).trim().split(' ');
-		const command = args.shift().toLowerCase();
+	if (message.content.startsWith(prefix + 'author')) {
+		const args = message.content.slice(7).trim().split(' ');
+		const bookName = args.shift().toLowerCase();
 
-		text = await fetchAuthor(command);
+		console.log(command);
+		text = await fetchAuthor(bookName);
 		message.reply(text);
 	}
 });
 
 bot.on("message", async message => {
-	if (message.content.startsWith(prefix + 'author', 0)) {
-		const args = message.content.slice(7).trim().split(' ');
-		const command = args.shift().toLowerCase();
+	if (message.content.startsWith(prefix + 'books', 0)) {
+		const args = message.content.slice(6).trim().split(' ');
+		const authorName = args.shift().toLowerCase();
 
-		text = await fetchBooks(command);
-		message.reply(text);
+		let array = await fetchBooks(authorName);
+		message.reply("\n1: " + array[0] + "\n" +
+			"2: " + array[1] + "\n" +
+			"3: " + array[2] + "\n" +
+			"4: " + array[3] + "\n" +
+			"5: " + array[4] + "\n");
 	}
 });
 
 async function fetchBooks(string) {
-	let response = await fetch('https://www.goodreads.com/search/index.xml?key=iFBuNgOuOt67dGewnaVPg&q=' + string);
+	let response = await fetch('https://www.goodreads.com/api/author_url/' + string + '?key=iFBuNgOuOt67dGewnaVPg');
 	let data = await response.text();
 
 	var xmlDoc = parser.parseFromString(data, "text/xml");
-	var x = xmlDoc.getElementsByTagName("results");
+	let authorId = xmlDoc.getElementsByTagName("author")[0].getAttribute("id").toString();
+	console.log("Author Id: " + authorId);
 
-	var resultParsed = parser.parseFromString(x[0].innerHTML, "text/xml")
-	let authorId = resultParsed.getElementsByTagName("author")[0].childNodes[1].textContent.toString();
+	// var x = xmlDoc.getElementsByTagName("results");
+	// var resultParsed = parser.parseFromString(x[0].innerHTML, "text/xml")
+	// let authorId = resultParsed.getElementsByTagName("author")[0].childNodes[1].textContent.toString();
 
 	response = await fetch('https://www.goodreads.com/author/list/' + authorId + '?format=xml&key=iFBuNgOuOt67dGewnaVPg');
 	data = await response.text();
@@ -47,17 +54,16 @@ async function fetchBooks(string) {
 	xmlDoc = parser.parseFromString(data, "text/xml");
 	x = xmlDoc.getElementsByTagName("author");
 
-	console.log(x.length);
-
-	for (i = 0; i < x.length; i++) {
-		console.log(x[i].getElementsByTagName("title")[0].childNodes[0].text);
+	let arr = [];
+	for (i = 0; i < 5; i++) {
+		arr[i] = x[0].childNodes[8].getElementsByTagName("title")[i].textContent;
 	}
 
-
+	return arr;
 }
 
-async function fetchAuthor(string) {
-	let response = await fetch('https://www.goodreads.com/search/index.xml?key=iFBuNgOuOt67dGewnaVPg&q=' + string)
+async function fetchAuthor(authorName) {
+	let response = await fetch('https://www.goodreads.com/search/index.xml?key=iFBuNgOuOt67dGewnaVPg&q=' + authorName)
 	let data = await response.text();
 
 	var xmlDoc = parser.parseFromString(data, "text/xml");
